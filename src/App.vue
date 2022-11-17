2025e46b1b2535d809ff7a4133b07823a3d22a76
Bankef <114829306+Bankef@users.noreply.github.com>
2022-11-17 15:52:16 -0600

    Lab completed with comments

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1>Would You Rather?</h1>
+
+    <would-you-rather
+       v-for="question in questions"
+       v-bind:id="question.id"
+       v-bind:question="question.question"
+       v-bind:answer1="question.answer1"
+       v-bind:answer2="question.answer2"
+       v-on:answer-changed="answerChanged"></would-you-rather>
+
+    <h2>Your answer</h2>
+
+    <p v-for="choice in choices">{{ choice }}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      choices: [],
+      //An array of questions and answers used for the app
+      questions: [
+        {
+          id: 0,
+          question: '...be an incredibly fast swimmer or an incredibly fast runner?',
+          answer1: 'Fast swimmer',
+          answer2: 'Fast runner',
+          },
+        {
+          id: 1,
+          question: '...be really good at math or really good at sports?',
+          answer1: 'Good at math',
+          answer2: 'Good at sports',
+          },
+        {
+          id: 2,
+          question: '...have the ability to see 10 minutes into the future or 10 years into the future?',
+          answer1: '10 minutes',
+          answer2: '10 years',
+        },
+      ],
+      userSelectionMessage:'' // Set to empty, expecting user input
+    }
+  },
+  methods: { //
+    answerChanged(id, choice) {
+      this.choices[id] = choice
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: darkgray;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wyr">
+
+    <h2>Would You Rather...</h2>
+
+    <h3>{{ question }}</h3>
+
+   <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+   <label>{{ answer1}}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2}}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    // The types of each input to expect from the parent app are listed here
+    id: Number,
+    question: String,
+    answer1: String,
+    answer2: String,
+    choices: []
+  },
+  data() {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+    //This transmits the users choice to the parent, app.vue and push the user's choice made for each question
+    choiceMade() {
+      this.$emit('answer-changed', this.id, this.choice)
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 25px;
+}
+
+.wyr {
+  border: 2px solid darkolivegreen;
+  background-color: burlywood;
+  margin: 10px;
+}
+</style>
+
